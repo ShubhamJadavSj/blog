71c82f0bf65fd254617b557d6432adfc13942bfc
Shubham Jadav <79038365+ShubhamJadavSj@users.noreply.github.com>
2022-08-23 22:00:51 +0530

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .copy('resources/fonts', 'public/fonts')
+    .sass('resources/css/_fonts.scss', 'public/css')
+    .copy('node_modules/@fortawesome/fontawesome-pro/webfonts', 'public/fonts')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
